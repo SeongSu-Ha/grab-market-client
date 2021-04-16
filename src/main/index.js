3a393138572eb7373250072aa9fc2b7a2fbb244b
SeongSu-Ha <0rkwhr0@gmail.com>
2021-04-16 17:12:47 +0900

    그랩 마켓 반응형 웹

--- src/main/index.js
+++ src/main/index.js
@@ -1,51 +1,79 @@
+import React from "react";
 import "./index.css";
 import axios from "axios";
-import React from "react";
 import { Link } from "react-router-dom";
-
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { API_URL } from "../config/constants.js";
+import { Carousel } from "antd";
+dayjs.extend(relativeTime);
 function MainPage() {
-  const [products, setProduct] = React.useState([]);
+  const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   React.useEffect(function () {
     axios
-      .get(
-        "https://e34ef501-31f4-45c0-973a-3f51ee2a26d9.mock.pstmn.io/products"
-      )
+      .get(`${API_URL}/products`)
       .then(function (result) {
         const products = result.data.products;
-        setProduct(products);
+        setProducts(products);
       })
       .catch(function (error) {
-        console.error("에러 발생: ", error);
+        console.error("에러 발생 : ", error);
+      });
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error("에러 발생 : ", error);
       });
   }, []);
   return (
     <div>
-      <div id="banner">
-        <img src="images/banners/banner1.png" />
-      </div>
-      <h1>판매되는 상품들</h1>
+      <Carousel autoplay autoplaySpeed={4500}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageUrl}`} />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
+      <h1 id="product-headline">판매되는 상품들</h1>
       <div id="product-list">
         {products.map(function (product, index) {
           return (
             <div className="product-card">
+              {product.soldout === 1 && <div className="product-blur" />}
               <Link
                 style={{ color: "inherit" }}
                 className="product-link"
                 to={`/products/${product.id}`}
               >
                 <div>
-                  <img className="product-img" src={product.imageUrl} />
+                  <img
+                    className="product-img"
+                    src={`${API_URL}/${product.imageUrl}`}
+                  />
                 </div>
-                <div className="product-content">
+                <div className="product-contents">
                   <span className="product-name">{product.name}</span>
-                  <br />
                   <span className="product-price">{product.price}원</span>
-                  <div className="product-seller">
-                    <img
-                      className="product-avatar"
-                      src="images/icons/avatar.png"
-                    />
-                    <span>{product.seller}</span>
+                  <div className="product-footer">
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                    <span className="product-date">
+                      {dayjs(product.createdAt).fromNow()}
+                    </span>
                   </div>
                 </div>
               </Link>
@@ -56,5 +84,4 @@ function MainPage() {
     </div>
   );
 }
-
 export default MainPage;
